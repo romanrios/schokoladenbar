91faf11ea0ba77a522dada60f8346cf8584bb11b
Roman Rios <romanrios@live.com>
2024-06-12 16:23:22 -0300

    02

---
+++ 02/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1718219899,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/angela-sheet0.png",
+		"images/bombon_azul-sheet0.png",
+		"images/bombon_rojo-sheet0.png",
+		"images/bombon_verde-sheet0.png",
+		"images/cinta-sheet0.png",
+		"images/spawn-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Project: Nur Schokoladen Game
 
-| Description | GitHub Pages |
-| ----- | ----- |
-| Test 01 | [Link](https://romanrios.github.io/nur-schokoladen/01)
+| Description | GitHub Pages                                           |
+| ----------- | ------------------------------------------------------ |
+| Test 01     | [Link](https://romanrios.github.io/nur-schokoladen/01) |
+| Test 02     | [Link](https://romanrios.github.io/nur-schokoladen/02) |
